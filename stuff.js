2f5b61b2ade7e63fcc55b7e5dfb12b39c2c5423f
Thomas Robinson <tom@diamondpigman.co.uk>
2016-07-29 06:22:11 +0100

    changed contact button a small bit

---
+++ stuff.js
@@ -0,0 +1,9 @@
+$("#em").click(function() {
+  $("#contact").fadeToggle();
+});
+if (window.top !== window.self) window.top.location.replace(window.self.location.href);
+console.log('______________________');
+console.log('| The page has loaded |');
+console.log('‾‾‾‾‾‾‾‾‾‾‾‾‾‾/‾‾‾‾‾‾‾');
+console.log('             /');
+console.log(' ¦̵̱ ̵̱ ̵̱ ̵̱ ̵̱(̢ ̡͇̅└͇̅┘͇̅ (▤8כ−◦');
