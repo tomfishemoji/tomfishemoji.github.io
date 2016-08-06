3ec0ab936c591f8191539eba546edb9abf4785d4
Thomas Robinson <tom@diamondpigman.co.uk>
2016-08-06 08:51:20 +0100

    why u nu fade

--- js/stuff.js
+++ js/stuff.js
@@ -11,7 +11,7 @@ $("#ho").click(function() {
   $("#about").delay(500).fadeIn(500);
   $("#em").delay(500).fadeIn(500);
 });
-$('#disclaimer').show().delay(2500).fadeOut(500);
+$('#disclaimer').delay(2500).fadeOut(500);
 
 //Regular JS
 if (window.top !== window.self) window.top.location.replace(window.self.location.href);
