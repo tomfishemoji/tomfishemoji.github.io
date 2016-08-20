4fca1ca34f59c1d928b6aa5e7f6aca413549ea27
Thomas Robinson <tom@diamondpigman.co.uk>
2016-08-20 08:51:01 +0100

    Add name animation and made logo smaller

--- js/stuff.js
+++ js/stuff.js
@@ -13,9 +13,40 @@ console.log("Quantum Fireball 540MB");
 console.log('-_-_-_-_-_-_-_-_-_-_-_');
 console.log('Type "help()" for a list of commands');
 function help() {
-   console.log('There is no help for you.');
-   window.setTimeout(rick,2500);
- }
- function rick() {
+  console.log('There is no help for you.');
+  window.setTimeout(rick,2500);
+}
+function rick() {
     window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-  }
+}
+
+function name() {
+    window.setTimeout(d,500);
+}
+function d() {
+    document.getElementById("title").innerHTML="{d}";
+    window.setTimeout(dn,100);
+}
+function dn() {
+  document.getElementById("title").innerHTML="{dn}";
+  window.setTimeout(dno,100);
+}
+function dno() {
+  document.getElementById("title").innerHTML="{dno}";
+  window.setTimeout(dnom,100);
+}
+function dnom() {
+  document.getElementById("title").innerHTML="{dnom}";
+  window.setTimeout(dnoma,100);
+}
+function dnoma() {
+  document.getElementById("title").innerHTML="{dnoma}";
+  window.setTimeout(dnomai,100);
+}
+function dnomai() {
+  document.getElementById("title").innerHTML="{dnomai}";
+  window.setTimeout(dnomaid,100);
+}
+function dnomaid() {
+  document.getElementById("title").innerHTML="{<span contenteditable>dnomaid</span>}";
+}
