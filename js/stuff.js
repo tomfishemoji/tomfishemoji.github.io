923d948991fbc224e17200f0ec57df56ad0cb770
Thomas Robinson <tom@diamondpigman.co.uk>
2016-10-05 22:19:11 +0100

    things

--- js/stuff.js
+++ js/stuff.js
@@ -1,29 +1,11 @@
 if (window.top !== window.self) window.top.location.replace(window.self.location.href);
 
-console.log('______________________');
-console.log('|  dnomaid DOS v1.0  |');
-console.log('‾‾‾‾‾‾‾‾‾‾‾‾‾‾/‾‾‾‾‾‾‾');
-console.log('             /');
-console.log(' ¦̵̱ ̵̱ ̵̱ ̵̱ ̵̱(̢ ̡͇̅└͇̅┘͇̅ (▤8כ−◦');
-console.log('-_-_-_-_-_-_-_-_-_-_-_');
-console.log("Intel i486SX 33MHz");
-console.log("12MB system RAM");
-console.log("Keyboard: Detected");
-console.log("Quantum Fireball 540MB");
-console.log('-_-_-_-_-_-_-_-_-_-_-_');
-console.log('Type "help()" for a list of commands');
+console.log('____________________');
+console.log('|  dnomaidOS v0.2  |');
+console.log('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾');
+console.log("Type 'help()' for a command list");
 function help() {
   console.log('There is no help for you.');
-  window.setTimeout(gummy,2500);
-}
-function gummy() {
-    document.write('<title>gummy bears are cool</title>');
-    document.write('<style>* { padding: 0; margin: 0; }</style>');
-    document.write('<iframe src="https://www.youtube.com/embed/astISOttCQ0?autoplay=1&controls=0&loop=1&showinfo=0&iv_load_policy=3&rel=0" frameborder="0" style="position: absoloute; width: 100%; height: 100%;"></iframe>');
-    console.log('Type "back()" to return to the site.');
-}
-function back() {
-  location.reload();
 }
 
 function name() {
