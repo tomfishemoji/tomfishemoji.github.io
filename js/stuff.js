76b19dfa5285c65bbb8f9b8e0b5b82e48fc030bd
Thomas Robinson <tom@diamondpigman.co.uk>
2016-10-10 04:31:46 +0100

    *small* update
    redesigned entire site

--- js/stuff.js
+++ js/stuff.js
@@ -9,38 +9,38 @@ function help() {
 }
 
 function name() {
-    window.setTimeout(brackets,500);
+    window.setTimeout(brackets,000);
 }
 function brackets() {
-    document.getElementById("title").innerHTML="{}";
+    document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>";
     window.setTimeout(d,100);
 }
 function d() {
-    document.getElementById("title").innerHTML="{d}";
+    document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>d}";
     window.setTimeout(dn,100);
 }
 function dn() {
-  document.getElementById("title").innerHTML="{dn}";
+  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dn}";
   window.setTimeout(dno,100);
 }
 function dno() {
-  document.getElementById("title").innerHTML="{dno}";
+  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dno}";
   window.setTimeout(dnom,100);
 }
 function dnom() {
-  document.getElementById("title").innerHTML="{dnom}";
+  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dnom}";
   window.setTimeout(dnoma,100);
 }
 function dnoma() {
-  document.getElementById("title").innerHTML="{dnoma}";
+  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dnoma}";
   window.setTimeout(dnomai,100);
 }
 function dnomai() {
-  document.getElementById("title").innerHTML="{dnomai}";
+  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dnomai}";
   window.setTimeout(dnomaid,100);
 }
 function dnomaid() {
-  document.getElementById("title").innerHTML="{dnomaid}";
+  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dnomaid}";
 }
 
 if (location.protocol == 'file:') {
