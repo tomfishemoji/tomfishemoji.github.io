3d68775f1640233f01041b4b3ee84f4d50aeaf31
Thomas Robinson <tom@dnomaid.co.uk>
2016-10-20 10:47:11 +0100

    remove bracket span

--- js/stuff.js
+++ js/stuff.js
@@ -12,35 +12,35 @@ function name() {
     window.setTimeout(brackets,000);
 }
 function brackets() {
-    document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>";
+    document.getElementById("title").innerHTML="{}";
     window.setTimeout(d,100);
 }
 function d() {
-    document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>d}";
+    document.getElementById("title").innerHTML="{d}";
     window.setTimeout(dn,100);
 }
 function dn() {
-  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dn}";
+  document.getElementById("title").innerHTML="{dn}";
   window.setTimeout(dno,100);
 }
 function dno() {
-  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dno}";
+  document.getElementById("title").innerHTML="{dno}";
   window.setTimeout(dnom,100);
 }
 function dnom() {
-  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dnom}";
+  document.getElementById("title").innerHTML="{dnom}";
   window.setTimeout(dnoma,100);
 }
 function dnoma() {
-  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dnoma}";
+  document.getElementById("title").innerHTML="{dnoma}";
   window.setTimeout(dnomai,100);
 }
 function dnomai() {
-  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dnomai}";
+  document.getElementById("title").innerHTML="{dnomai}";
   window.setTimeout(dnomaid,100);
 }
 function dnomaid() {
-  document.getElementById("title").innerHTML="<span class=\"bracket\">{</span>dnomaid}";
+  document.getElementById("title").innerHTML="{dnomaid}";
 }
 
 var textArray = [
