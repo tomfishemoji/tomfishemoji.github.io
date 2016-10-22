698108661ca6b3febdcd4f7c665a3acac4288a4e
Thomas Robinson <tom@dnomaid.co.uk>
2016-10-22 15:10:59 +0100

    many things changed/updated/added/removed

--- js/stuff.js
+++ js/stuff.js
@@ -8,41 +8,6 @@ function help() {
   console.log('There is no help for you.');
 }
 
-function name() {
-    window.setTimeout(brackets,000);
-}
-function brackets() {
-    document.getElementById("title").innerHTML="{}";
-    window.setTimeout(d,100);
-}
-function d() {
-    document.getElementById("title").innerHTML="{d}";
-    window.setTimeout(dn,100);
-}
-function dn() {
-  document.getElementById("title").innerHTML="{dn}";
-  window.setTimeout(dno,100);
-}
-function dno() {
-  document.getElementById("title").innerHTML="{dno}";
-  window.setTimeout(dnom,100);
-}
-function dnom() {
-  document.getElementById("title").innerHTML="{dnom}";
-  window.setTimeout(dnoma,100);
-}
-function dnoma() {
-  document.getElementById("title").innerHTML="{dnoma}";
-  window.setTimeout(dnomai,100);
-}
-function dnomai() {
-  document.getElementById("title").innerHTML="{dnomai}";
-  window.setTimeout(dnomaid,100);
-}
-function dnomaid() {
-  document.getElementById("title").innerHTML="{dnomaid}";
-}
-
 var textArray = [
     'Tweets too much',
     'Makes computer go beep boop',
