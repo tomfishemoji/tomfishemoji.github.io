e831a0e3dab6205b6e4bc2dfad7c8deb329541a2
Thomas Robinson <tom@dnomaid.co.uk>
2016-11-13 09:32:01 +0000

    more/less button & JS stuff

--- js/stuff.js
+++ js/stuff.js
@@ -1,3 +1,17 @@
+var showhide = document.getElementById('more');
+
+document.getElementById('more').onclick = function() {
+    document.getElementById('morelinks').style.display = 'block';
+    document.getElementById('more').style.display = 'none';
+    document.getElementById('less').style.display = 'inline';
+};
+
+document.getElementById('less').onclick = function() {
+    document.getElementById('morelinks').style.display = 'none';
+    document.getElementById('more').style.display = 'inline';
+    document.getElementById('less').style.display = 'none';
+};
+
 if (window.top !== window.self) window.top.location.replace(window.self.location.href);
 
 if (location.protocol == 'file:') {
