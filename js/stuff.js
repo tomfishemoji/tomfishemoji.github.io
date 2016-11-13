e5693459e2c5f62b4d2558b775e7e09b30117e05
Thomas Robinson <tom@dnomaid.co.uk>
2016-11-13 10:26:30 +0000

    more links

--- js/stuff.js
+++ js/stuff.js
@@ -2,14 +2,16 @@ var showhide = document.getElementById('more');
 
 document.getElementById('more').onclick = function() {
     document.getElementById('morelinks').style.display = 'block';
+    document.getElementById('mainlinks').style.display = 'none';
     document.getElementById('more').style.display = 'none';
-    document.getElementById('less').style.display = 'inline';
+    document.getElementById('back').style.display = 'inline';
 };
 
-document.getElementById('less').onclick = function() {
+document.getElementById('back').onclick = function() {
     document.getElementById('morelinks').style.display = 'none';
+    document.getElementById('mainlinks').style.display = 'block';
     document.getElementById('more').style.display = 'inline';
-    document.getElementById('less').style.display = 'none';
+    document.getElementById('back').style.display = 'none';
 };
 
 if (window.top !== window.self) window.top.location.replace(window.self.location.href);
