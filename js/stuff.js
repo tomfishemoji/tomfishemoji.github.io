6177687769b9310458f093244ee3cf70f8fbbf70
Thomas Robinson <tom@dnomaid.co.uk>
2016-11-13 16:55:08 +0000

    even more

--- js/stuff.js
+++ js/stuff.js
@@ -3,15 +3,23 @@ var showhide = document.getElementById('more');
 document.getElementById('more').onclick = function() {
     document.getElementById('morelinks').style.display = 'block';
     document.getElementById('mainlinks').style.display = 'none';
-    document.getElementById('more').style.display = 'none';
-    document.getElementById('back').style.display = 'inline';
 };
 
 document.getElementById('back').onclick = function() {
     document.getElementById('morelinks').style.display = 'none';
     document.getElementById('mainlinks').style.display = 'block';
-    document.getElementById('more').style.display = 'inline';
-    document.getElementById('back').style.display = 'none';
+};
+
+document.getElementById('evenmore').onclick = function() {
+    document.getElementById('morelinks').style.display = 'none';
+    document.getElementById('mainlinks').style.display = 'none';
+    document.getElementById('evenmorelinks').style.display = 'block';
+};
+
+document.getElementById('backback').onclick = function() {
+    document.getElementById('morelinks').style.display = 'block';
+    document.getElementById('mainlinks').style.display = 'none';
+    document.getElementById('evenmorelinks').style.display = 'none';
 };
 
 if (window.top !== window.self) window.top.location.replace(window.self.location.href);
