e5d00db2f532bff71fe16d8d7864c4869dec430d
Thomas Robinson <tom@dnomaid.co.uk>
2016-11-19 17:51:33 +0000

    remove 2nd page of links

--- js/stuff.js
+++ js/stuff.js
@@ -10,18 +10,6 @@ document.getElementById('back').onclick = function() {
     document.getElementById('mainlinks').style.display = 'block';
 };
 
-document.getElementById('evenmore').onclick = function() {
-    document.getElementById('morelinks').style.display = 'none';
-    document.getElementById('mainlinks').style.display = 'none';
-    document.getElementById('evenmorelinks').style.display = 'block';
-};
-
-document.getElementById('backback').onclick = function() {
-    document.getElementById('morelinks').style.display = 'block';
-    document.getElementById('mainlinks').style.display = 'none';
-    document.getElementById('evenmorelinks').style.display = 'none';
-};
-
 if (window.top !== window.self) window.top.location.replace(window.self.location.href);
 
 if (location.protocol == 'file:') {
