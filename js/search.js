f1d397e35891f79ca83f354b9bf748dd10f8a682
tom <tom@dnomaid.co.uk>
2017-10-24 00:28:18 +0100

    Add search box to 404 page

--- js/search.js
+++ js/search.js
@@ -2,12 +2,18 @@ if (window.location.hash) {
   document.getElementById('search-input').value = window.location.hash.replace('#', '');
 }
 
+if (document.getElementById('search-js').getAttribute('data-limit')) {
+  rlimit = document.getElementById('search-js').getAttribute('data-limit');
+} else {
+  rlimit = 5;
+}
+
 window.simpleJekyllSearch = new SimpleJekyllSearch({
   searchInput: document.getElementById('search-input'),
   resultsContainer: document.getElementById('search-results'),
   json: '/js/posts.json',
   searchResultTemplate: '<li><h4>{date} in {category}{author}</h4><h2><a href="{url}">{title}</a></h2></li>',
   noResultsText: '<h4 style="padding-bottom: 16px;">No results</h4>',
-  limit: 5,
+  limit: rlimit,
   fuzzy: false
 })
