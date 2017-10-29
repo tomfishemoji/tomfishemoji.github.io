bf22eef19659afd91f2e3fa09240c5f1735f9acd
tom <tom@dnomaid.co.uk>
2017-10-29 01:51:19 +0100

    Projects page now shows repos on others' accounts

--- js/projects.js
+++ js/projects.js
@@ -6,7 +6,7 @@ function addZero(what) {
   }
 }
 
-apiurl = 'https://api.github.com/users/mrdnomaid/repos?per_page=100';
+apiurl = 'https://api.github.com/users/mrdnomaid/repos?type=all&per_page=100';
 exclude = [
   93560944,
   64045777,
@@ -14,6 +14,7 @@ exclude = [
   64804610,
   64443395
 ];
+var e;
 
 $(document).ready(function() {
   try {
@@ -37,22 +38,34 @@ $(document).ready(function() {
               buttons += '  <a class="button" href="' + response[i].homepage + '" target="_blank" rel="noopener noreferrer">Live Site</a>';
             }
 
-            if (response[i].homepage) {
-              lang = response[i].language.replace('CSS', 'HTML').replace(' ', '-');
-              langd = response[i].language.replace('CSS', 'HTML');
+            if (response[i].language) {
+              if (response[i].homepage) {
+                lang = response[i].language.replace('CSS', 'HTML').replace(' ', '-');
+                langd = response[i].language.replace('CSS', 'HTML');
+              } else {
+                lang = response[i].language.replace(' ', '-');
+                langd = response[i].language;
+              }
             } else {
-              lang = response[i].language.replace(' ', '-');
-              langd = response[i].language;
+              lang = 'none';
+              langd = 'None';
             }
 
             dateo = new Date(response[i].pushed_at);
             date = dateo.getFullYear() + '-' + addZero(dateo.getMonth()) + '-' + addZero(dateo.getDate());
 
-            document.getElementById('github-results').innerHTML += '<h2 class="invert" id="' + response[i].id + '" title="' + response[i].id + '">' + response[i].name + '</h2>';
-            document.getElementById('github-results').innerHTML += '<h4 style="font-size: 1em;">' + description + '</h4>';
-            document.getElementById('github-results').innerHTML += '<h4 style="font-size: 1em;">' + buttons + ' <span class="button lang ' + lang + '" title="Most used language, according to GitHub">' + langd + '</span> <span title="Last Updated">' + date + '</span></h4>';
+            if (response[i].full_name.includes('mrdnomaid/')) {
+              document.getElementById('github-results').innerHTML += '<h2 class="invert" id="' + response[i].id + '" title="' + response[i].id + '">' + response[i].name + '</h2>';
+              document.getElementById('github-results').innerHTML += '<h4 style="font-size: 1em;">' + description + '</h4>';
+              document.getElementById('github-results').innerHTML += '<h4 style="font-size: 1em;">' + buttons + ' <span class="button lang ' + lang + '" title="Most used language, according to GitHub">' + langd + '</span> <span title="Last Updated">' + date + '</span></h4>';
+            } else {
+              e += '<h2 class="invert" id="' + response[i].id + '" title="' + response[i].id + '">' + response[i].full_name + '</h2> <h4 style="font-size: 1em;">' + description + '</h4><h4 style="font-size: 1em;">' + buttons + ' <span class="button lang ' + lang + '" title="Most used language, according to GitHub">' + langd + '</span> <span title="Last Updated">' + date + '</span></h4>';
+              e = e.replace('undefined', '');
+            }
           }
         }
+        document.getElementById('github-results').innerHTML += '<h1 style="margin-top: 64px;margin-bottom: 16px;border-bottom: 2px solid #002e59;">Elsewhere</h1>';
+        document.getElementById('github-results').innerHTML += e;
       }
     });
   } catch (err) {
