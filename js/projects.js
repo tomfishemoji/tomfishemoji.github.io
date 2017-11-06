a2357d6e505eb2fb976cce51db834d50b8efbfb6
Tom (dnomaid) <tom@dnomaid.co.uk>
2017-11-06 22:00:08 +0000

    Redesign projects page a bit

--- js/projects.js
+++ js/projects.js
@@ -31,36 +31,38 @@ $(document).ready(function() {
               description = response[i].description.replace(/:.*:/, '');
             }
 
-            if (!response[i].homepage) {
-              buttons = '<a class="button" href="' + response[i].html_url + '" target="_blank" rel="noopener noreferrer">Repo</a>';
+            if (response[i].homepage) {
+              lnka = '<a href="' + response[i].homepage + '">';
+              lnkb = '</a>';
             } else {
-              buttons = '<a class="button" href="' + response[i].html_url + '" target="_blank" rel="noopener noreferrer">Repo</a>';
-              buttons += '  <a class="button" href="' + response[i].homepage + '" target="_blank" rel="noopener noreferrer">Live Site</a>';
+              lnka = '<a href="' + response[i].html_url + '">';
+              lnkb = '</a>';
             }
 
-            if (response[i].language) {
-              if (response[i].homepage) {
-                lang = response[i].language.replace('CSS', 'HTML').replace(' ', '-');
-                langd = response[i].language.replace('CSS', 'HTML');
-              } else {
-                lang = response[i].language.replace(' ', '-');
-                langd = response[i].language;
-              }
-            } else {
-              lang = 'none';
-              langd = 'None';
-            }
+            // if (response[i].language) {
+            //   if (response[i].homepage) {
+            //     lang = response[i].language.replace('CSS', 'HTML').replace(' ', '-');
+            //     langd = response[i].language.replace('CSS', 'HTML');
+            //   } else {
+            //     lang = response[i].language.replace(' ', '-');
+            //     langd = response[i].language;
+            //   }
+            // } else {
+            //   lang = 'none';
+            //   langd = 'None';
+            // }
 
             dateo = new Date(response[i].pushed_at);
             date = dateo.getFullYear() + '-' + addZero(dateo.getMonth()) + '-' + addZero(dateo.getDate());
 
             if (response[i].full_name.includes('mrdnomaid/')) {
-              document.getElementById('github-results').innerHTML += '<h2 class="invert" id="' + response[i].id + '" title="' + response[i].id + '">' + response[i].name + '</h2>';
-              document.getElementById('github-results').innerHTML += '<h4 style="font-size: 1em;">' + description + '</h4>';
-              document.getElementById('github-results').innerHTML += '<h4 style="font-size: 1em;">' + buttons + ' <span class="button lang ' + lang + '" title="Most used language, according to GitHub">' + langd + '</span> <span title="Last Updated">' + date + '</span></h4>';
+              // Bit for the languages :
+              // <span class="button lang ' + lang + '" title = "Most used language, according to GitHub">' + langd + '</span>'
+
+              document.getElementById('github-results').innerHTML += '<b>' + lnka + response[i].name + lnkb + '</b> &mdash; <i class="desc">' + description + '&nbsp;<span title="Last Updated">(' + date + ')</span></i><p>';
+
             } else {
-              e += '<h2 class="invert" id="' + response[i].id + '" title="' + response[i].id + '">' + response[i].full_name + '</h2> <h4 style="font-size: 1em; margin-bottom: 4px;">' + description + '</h4><h4 style="font-size: 1em;">' + buttons + ' <span class="button lang ' + lang + '" title="Most used language, according to GitHub">' + langd + '</span> <span title="Last Updated">' + date + '</span></h4>';
-              e = e.replace('undefined', '');
+              e += '<b>' + lnka + response[i].name + lnkb + '</b> &mdash; <i class="desc">' + description + '&nbsp;<span title="Last Updated">(' + date + ')</span></i><p>';
             }
           }
         }
@@ -71,6 +73,5 @@ $(document).ready(function() {
   } catch (err) {
     document.getElementById('loading').innerHTML = 'Something went wrong';
     document.getElementById('loading').innerHTML += err;
-
   }
 });
