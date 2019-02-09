193ae5e81a9a3ed7cfa1389de101305e52972f6d
Tom <tom@dnomaid.co.uk>
2019-02-09 18:05:45 +0000

    Add projects page back

---
+++ js/pages/projects-list.js
@@ -0,0 +1,212 @@
+const projects = [{
+    "name": "tomr.me",
+    "desc": "Jekyll site that you are looking at right now",
+    "url": "https://tomr.me",
+    "gh": "mrdnomaid/mrdnomaid.github.io"
+  },
+
+  {
+    "name": "Brexit",
+    "desc": "The obviously-not-fictitious website for HM Government's Department for Brexit and Trade Negotiations",
+    "url": "/brexit",
+    "gh": "mrdnomaid/brexit"
+  },
+
+  {
+    "name": "User Scripts",
+    "desc": "Bodged together scripts for websites to add/fix functionality",
+    "url": false,
+    "gh": "mrdnomaid/userscripts"
+  },
+
+  {
+    "name": "User Styles",
+    "desc": "Hastilly made CSS to tweak elements on websites",
+    "url": false,
+    "gh": "mrdnomaid/userstyles"
+  },
+
+  {
+    "name": "Pinghost Redesign",
+    "desc": "A redesigned website for a now-defunct hosting company",
+    "url": "https://ping.dnomaid.co.uk",
+    "gh": "mrdnomaid/userstyles"
+  },
+
+  {
+    "name": "Tools",
+    "desc": "Small, helpful utilities",
+    "url": "/tools",
+    "gh": "mrdnomaid/tools"
+  },
+
+  {
+    "name": "redditunsubinactive",
+    "desc": "Unsubscribe from inactive subreddits",
+    "url": "/redditunsubinactive",
+    "gh": "mrdnomaid/redditunsubinactive"
+  },
+
+  {
+    "name": "Scripts",
+    "desc": "Useful scripts for automating things I do often enough that they require a script to automate them ",
+    "url": false,
+    "gh": "mrdnomaid/scripts"
+  },
+
+  {
+    "name": "Next Train",
+    "desc": "Google Assistant application that will tell you the next trains departing from any UK railway station",
+    "url": "https://assistant.google.com/services/a/uid/0000002b9e81557d",
+    "gh": null
+  },
+
+  {
+    "name": "Hash",
+    "desc": "An attempt at making a Discord bot. It didn't work too well.",
+    "url": null,
+    "gh": "mrdnomaid/hash2"
+  },
+
+  {
+    "name": "WANCheck",
+    "desc": "Is meant to tell you when your internet goes down, but doesn't work that well either",
+    "url": "/wancheck",
+    "gh": "mrdnomaid/wancheck"
+  },
+
+  {
+    "name": "abeceda",
+    "desc": "Choose an artist, see which letters most commonly start their song titles",
+    "url": "/abeceda",
+    "gh": null
+  },
+
+  {
+    "name": "fa-chess",
+    "desc": "Saw that FontAwesome had some chess icons, so I made a little board from them.",
+    "url": "/fa-chess",
+    "gh": "mrdnomaid/fa-chess"
+  },
+
+  {
+    "name": "PHPcmd",
+    "desc": "Quickly made webpage to execute shell commands using PHP.",
+    "url": null,
+    "gh": "mrdnomaid/PHPcmd"
+  },
+
+  {
+    "name": "Ubuntu Codename Generator",
+    "desc": "Generate Plausible Ubuntu-like Codenames",
+    "url": "/ubuntucodenamegen",
+    "gh": "mrdnomaid/ubuntucodenamegen"
+  },
+
+  {
+    "name": "HP Printer Trees",
+    "desc": "See how many trees you've felled in the web UI of your HP printer",
+    "url": null,
+    "gh": "mrdnomaid/trees"
+  },
+
+  {
+    "name": "Reddit Image Gallery",
+    "desc": "A simple gallery to show images posted to a subreddit",
+    "url": "/redditgallery",
+    "gh": "mrdnomaid/redditgallery"
+  },
+
+  {
+    "name": "Download Upvoted Images",
+    "desc": "Download images you've upvoted on reddit",
+    "url": null,
+    "gh": "mrdnomaid/downloadupvotedimages"
+  },
+
+  {
+    "name": "FontRandom",
+    "desc": "Make some text look stupid.",
+    "url": "/fontrandom",
+    "gh": "mrdnomaid/fontrandom"
+  },
+
+  {
+    "name": "Random Colours",
+    "desc": "Remake of a PHP thing that just cycles through random hex codes",
+    "url": "/randomcolours",
+    "gh": "mrdnomaid/randomcolours"
+  },
+
+  {
+    "name": "BSODs",
+    "desc": "Some recreated Windows BSODs",
+    "url": "/bsods",
+    "gh": "mrdnomaid/bsods"
+  },
+
+  {
+    "name": "Steam Wishlist Total",
+    "desc": "Top up the cost of everything on your Steam wishlist",
+    "url": null,
+    "gh": "mrdnomaid/steamwishlisttotal"
+  },
+
+  {
+    "name": "Discord Downloads",
+    "desc": "Quick-links to Discord downloads",
+    "url": "/discorddownloads",
+    "gh": "mrdnomaid/discorddownloads"
+  },
+
+  {
+    "name": "WordFall",
+    "desc": "Some falling words",
+    "url": "/wordfall",
+    "gh": "mrdnomaid/wordfall"
+  },
+
+  {
+    "name": "NerdCubed Soundboard",
+    "desc": "Featuring reddit's favourite clips",
+    "url": "/n3soundboard",
+    "gh": "mrdnomaid/n3soundboard"
+  },
+
+  {
+    "name": "JavaScript Test",
+    "desc": "See if you have JavaScript enabled in your browser",
+    "url": "/jstest",
+    "gh": "mrdnomaid/jstest"
+  },
+
+  {
+    "name": "Windows",
+    "desc": "A website that looks like Windows 10",
+    "url": "/windows",
+    "gh": "mrdnomaid/windows"
+  },
+
+  {
+    "name": "OS X",
+    "desc": "A website that looks like OS X",
+    "url": "/mac",
+    "gh": "mrdnomaid/macos"
+  },
+
+  // Not on GH/mrdnomaid
+
+  {
+    "name": "Aurora Community",
+    "desc": "Aurora Community memorial website",
+    "url": "http://auroracommunity.io",
+    "gh": "AuroraCommunity/AuroraWebsite"
+  },
+
+  {
+    "name": "Melonz Website",
+    "desc": "The website for Melonz",
+    "url": "https://melonz.app/",
+    "git": "https://gitlab.com/Melonz/melonz.gitlab.io"
+  }
+]
---
+++ js/pages/projects.js
@@ -0,0 +1,71 @@
+const shuffledProjects = shuffle(projects);
+const div = document.getElementById('js-list');
+
+function load(shuffled) {
+  let output = '';
+  let toUse = projects;
+  if(shuffled) {
+    toUse = shuffledProjects;
+  }
+
+  for (let project of toUse) {
+    let thisOut = ``;
+
+    let thisName = project['name'];
+    let thisDesc = project['desc'];
+    let thisURL = project['url'];
+    let thisGitHub = project['gh'];
+    let thisGit = project['git'];
+
+    let link = '';
+    if (thisURL) {
+      link = `<a href="${thisURL}" target="_blank" rel="noopener" class="w"><i class="fas fa-fw fa-link"></i></a>`;
+    }
+
+    let git = '';
+    if (thisGitHub) {
+      git = `<a href="https://github.com/${thisGitHub}" target="_blank" rel="noopener" class="g"><i class="fab fa-fw fa-github"></i></a>`;
+    } else if (thisGit) {
+      git = `<a href="${thisGit}" target="_blank" rel="noopener" class="ge"><i class="fab fa-fw fa-git"></i></a>`;
+    }
+
+    thisOut = `
+          <li>
+            <b>${thisName}</b>
+            ${thisDesc}<br>
+            ${link} ${git}
+          </li>
+      `;
+
+    output += thisOut;
+  }
+
+  div.innerHTML = output;
+}
+
+load(true);
+
+
+
+
+
+// Fisher-Yates Shuffle, nicked from StackOverflow
+// https://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
