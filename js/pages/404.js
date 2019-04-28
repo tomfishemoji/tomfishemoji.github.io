2f7dd41a49c61505081b11e9835872ca07c266bc
Tom <tom@dnomaid.co.uk>
2019-04-28 03:10:09 +0100

    add directory listing

---
+++ js/pages/404.js
@@ -0,0 +1,31 @@
+const div = document.getElementById('js-folder');
+let files = '';
+let folders = '';
+let out = `<h3><i class="fas fa-fw fa-stream"></i> File Listing of ${window.location.pathname}</h3><p>Due to the way GitHub Pages handles folders, what should be a directory listing is actually turned into a 404 page. Below is a list of what is inside of ${window.location.pathname}.</p>`;
+
+$.getJSON(`https://api.github.com/repos/mrdnomaid/mrdnomaid.github.io/contents${window.location.pathname}`, function(j) {
+
+  for (file of j) {
+    if (file.type == 'file') {
+      files += `<a href="/${file.path}"><i class="fas fa-fw fa-file"></i> ${file.name}</a><br>`;
+    } else {
+      folders += `<a href="/${file.path}#open"><i class="fas fa-fw fa-folder"></i> ${file.name}</a><br>`;
+    }
+  }
+
+  out += folders;
+  out += files;
+  div.innerHTML = out;
+
+});
+
+if (window.location.hash.includes('open')) {
+  document.getElementById('js-folder').style.display = 'block';
+}
+
+document.getElementById('js-folder-trigger').style.display = 'block';
+
+function showList() {
+  document.getElementById('js-folder').style.display = 'block';
+  document.getElementById('js-folder-trigger').style.display = 'none';
+}
