81db58b1f1c85c9b93a9b5abf770019fcbb8f914
Tom <tom@dnomaid.co.uk>
2021-06-30 17:44:43 +0100

    add new landing page for visitors from old/archived sites

---
+++ js/pages/hellofrom.js
@@ -0,0 +1,10 @@
+const hashNoProtocol = window.location.hash.replace(/\#/,'').replace(/(http)s*\:\/\//g,'');
+
+if(hashNoProtocol.length > 1) {
+    const beforeSlash = hashNoProtocol.split('/')[0];
+    const afterSlash = (hashNoProtocol.split('/')[1]) ? hashNoProtocol.split(/\/(.+)/)[1] : '';
+
+    document.getElementById('js-site').innerHTML = `(<code>${beforeSlash}/${(afterSlash)}</code>)`;
+    document.getElementById('js-archive-link').href = `https://archive.trobinson.me/${beforeSlash}/${afterSlash}`;
+    document.getElementById('js-this-site-link').href = `/${afterSlash}`;
+}
