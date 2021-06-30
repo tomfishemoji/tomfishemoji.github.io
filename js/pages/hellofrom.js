fe4a7bca75cdf357ccffeeb33db42420adee19aa
Tom <tom@dnomaid.co.uk>
2021-06-30 18:00:48 +0100

    wording

--- js/pages/hellofrom.js
+++ js/pages/hellofrom.js
@@ -4,7 +4,7 @@ if(hashNoProtocol.length > 1) {
     const beforeSlash = hashNoProtocol.split('/')[0];
     const afterSlash = (hashNoProtocol.split('/')[1]) ? hashNoProtocol.split(/\/(.+)/)[1] : '';
 
-    document.getElementById('js-site').innerHTML = `(<code>${beforeSlash}/${(afterSlash)}</code>)`;
+    document.getElementById('js-site').innerHTML = `&mdash;<code>${beforeSlash}/${(afterSlash)}</code>&mdash;`;
     document.getElementById('js-archive-link').href = `https://archive.trobinson.me/${beforeSlash}/${afterSlash}`;
     document.getElementById('js-this-site-link').href = `/${afterSlash}`;
 }
